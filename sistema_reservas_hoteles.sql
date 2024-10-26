61f68df7f450b9202165876a419cd339241fb5d4
Omar Parada <omarparada89@outlook.com>
2024-10-26 00:22:17 -0500

    Insercion de los datos

--- sistema_reservas_hoteles.sql
+++ sistema_reservas_hoteles.sql
@@ -40,3 +40,19 @@ CREATE TABLE reserrvas(
     FOREIGN KEY (cliente_id) REFERENCES clientes(cliente_id),
     FOREIGN KEY (habitacion_id) REFERENCES habitaciones(habitaciones_id)
 );
+
+--INSERCION DE DATOS
+
+INSERT INTO clientes (nombre, direccion, telefono, correo) VALUES ('Ana maria','calle 123 falsa','12345678','anam@gmail.com'),
+    ('Juan Perez','calle 456 verdadera','98765432','juanp@gmail.com');
+
+INSERT INTO habitaciones (tipo, capacidad, precio) VALUES ('Simple',2,150.00),
+    ('Doble',4,250.00),
+    ('Triple',6,350.00);
+
+    INSERT INTO empleados (nombre, puesto, telefono, correo) VALUES ('Juan Pablo','Gerente','12345678','juanp@gmail.com'),
+    ('Maria Ana','Recepcionista','98765432','mariaa@gmail.com');
+
+    INSERT INTO reservas (cliente_id, habitacion_id, fecha_entrega, fecha_salida) VALUES (1,1,'2022-01-01','2022-01-15'),
+    (2,2,'2022-02-01','2022-02-10');
+    
